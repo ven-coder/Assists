3b80cdf765809775eaf38c456e22edbbdeadb383
Ven <ven_mail@163.com>
2024-05-02 19:20:29 +0800

    feat: 集成opencv

--- simple/src/main/java/com/ven/assists/simple/MainActivity.kt
+++ simple/src/main/java/com/ven/assists/simple/MainActivity.kt
@@ -2,8 +2,12 @@ package com.ven.assists.simple
 
 import android.os.Bundle
 import android.view.KeyEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.blankj.utilcode.util.BarUtils
+import com.blankj.utilcode.util.LogUtils
+import com.blankj.utilcode.util.PathUtils
+import com.blankj.utilcode.util.ResourceUtils
 import com.ven.assists.Assists
 import com.ven.assists.AssistsService
 import com.ven.assists.AssistsServiceListener
@@ -11,9 +15,18 @@ import com.ven.assists.simple.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
+import org.opencv.android.OpenCVLoader
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Point
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
 
 
 class MainActivity : AppCompatActivity(), AssistsServiceListener {
@@ -67,8 +80,66 @@ class MainActivity : AppCompatActivity(), AssistsServiceListener {
         BarUtils.setStatusBarLightMode(this, true)
         setContentView(viewBind.root)
         Assists.serviceListeners.add(this)
+
+        if (OpenCVLoader.initLocal()) {
+            LogUtils.d("OpenCV loaded successfully")
+        } else {
+            LogUtils.d("OpenCV initialization failed!")
+            Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            flow<Point?> {
+
+                ResourceUtils.copyFileFromAssets("a.jpg", PathUtils.getInternalAppCachePath() + "/a.jpg")
+                ResourceUtils.copyFileFromAssets("b.jpg", PathUtils.getInternalAppCachePath() + "/b.jpg")
+
+                val point = match(PathUtils.getInternalAppCachePath() + "/a.jpg", PathUtils.getInternalAppCachePath() + "/b.jpg");
+
+
+                emit(point)
+
+
+            }.flowOn(Dispatchers.IO)
+                .onCompletion {
+                    LogUtils.d(it)
+                }
+                .catch {
+                    LogUtils.d(it)
+                }
+                .collect {
+                    LogUtils.d(it)
+                }
+        }
     }
 
+    fun match(small: String, big: String): Point? {
+        try {
+            val bigMat = Imgcodecs.imread(big)
+            val templateMat = Imgcodecs.imread(small)
+            if (bigMat.empty()) {
+                System.out.println("")
+            }
+            if (bigMat.empty()) {
+                System.out.println("")
+            }
+
+            val method = Imgproc.TM_CCORR_NORMED
+            val width = bigMat.cols() - templateMat.cols() + 1
+            val height = bigMat.rows() - templateMat.rows() + 1
+            val result = Mat(width, height, CvType.CV_32FC1)
+            val currentTimeMillis = System.currentTimeMillis()
+            Imgproc.matchTemplate(bigMat, templateMat, result, method)
+            Core.normalize(result, result, 0.0, 1.0, Core.NORM_MINMAX, -1, Mat())
+            val mmr = Core.minMaxLoc(result)
+            val value = System.currentTimeMillis() - currentTimeMillis
+            LogUtils.d(value)
+            return mmr.maxLoc
+        } catch (cause: Throwable) {
+            throw cause
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
--- simple/src/main/java/com/ven/assists/simple/step/ScrollContacts.kt
+++ simple/src/main/java/com/ven/assists/simple/step/ScrollContacts.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.accessibility.AccessibilityNodeInfo
 import com.ven.assists.Assists
 import com.ven.assists.Assists.click
+import com.ven.assists.Assists.findFirstParentClickable
 import com.ven.assists.Assists.getBoundsInScreen
 import com.ven.assists.simple.App
 import com.ven.assists.simple.OverManager
@@ -13,9 +14,13 @@ import com.ven.assists.stepper.StepData
 import com.ven.assists.stepper.StepImpl
 import com.ven.assists.stepper.StepManager
 
+/**
+ * ScrollContacts为该业务场景的分类，作用是可在执行过程按按业务分类来执行
+ */
 class ScrollContacts : StepImpl {
     override fun onImpl(collector: StepCollector) {
-        collector.next(Step.STEP_1) {
+        //1. 打开微信
+        collector.next(Step.STEP_1) {//Step.STEP_1为自己定义的常量
             OverManager.log("启动微信")
             Intent().apply {
                 addCategory(Intent.CATEGORY_LAUNCHER)
@@ -23,18 +28,22 @@ class ScrollContacts : StepImpl {
                 component = ComponentName("com.tencent.mm", "com.tencent.mm.ui.LauncherUI")
                 Assists.service?.startActivity(this)
             }
+            //执行下一步Step.STEP_2，this::class.java为当前StepImpl实现类的步骤逻辑，如果传其他的StepImpl就会执行指定的StepImpl逻辑
             StepManager.execute(this::class.java, Step.STEP_2)
-        }.nextLoop(Step.STEP_2) {
+
+        }.nextLoop(Step.STEP_2) {//2. 定义Step.STEP_2逻辑，nextLoop该方法会在指定时间内按指定间隔循环执行
             OverManager.log("检查是否已打开微信主页：\n剩余时间=${it.loopSurplusSecond}秒")
+            //按文本查找元素
             Assists.findByText("通讯录").forEach {
                 val screen = it.getBoundsInScreen()
                 if (screen.left > Assists.getX(1080, 340) &&
                     screen.top > Assists.getX(1920, 1850)
                 ) {
                     OverManager.log("已打开微信主页，点击【通讯录】")
-                    it.parent.parent.click()
+                    it.findFirstParentClickable()?.click()
+                    //执行步骤STEP_3，data为自定义数据
                     StepManager.execute(this::class.java, Step.STEP_3, data = StepData(data = 1))
-                    return@nextLoop true
+                    return@nextLoop true //返回true表示结束循环检查
                 }
             }
 
@@ -47,10 +56,10 @@ class ScrollContacts : StepImpl {
             if (0f == it.loopSurplusSecond) {
                 StepManager.execute(this::class.java, Step.STEP_1)
             }
-
-            false
-        }.next(Step.STEP_3) { step ->
+            false//false表示继续循环
+        }.next(Step.STEP_3) { step ->//3. 定义Step.STEP_3逻辑
             OverManager.log("滚动下一页：${step.data}")
+            //
             Assists.findByTags("android.widget.ListView").forEach {
                 val screen = it.getBoundsInScreen()
                 if (screen.left >= 0 && screen.left < Assists.getX(1080, 1080) &&
