fe6ca789f9a2037c3d0a8cedd24c06ac6717bbd7
ven <ven.coder.ing@gmail.com>
2024-08-05 20:28:44 +0800

    20%

---
+++ sample-host/src/main/java/com/tencent/shadow/sample/host/MainActivity.java
@@ -0,0 +1,104 @@
+/*
+ * Tencent is pleased to support the open source community by making Tencent Shadow available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.tencent.shadow.sample.host;
+
+import static com.tencent.shadow.sample.constant.Constant.PART_KEY_PLUGIN_BASE;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.tencent.shadow.sample.constant.Constant;
+import com.tencent.shadow.sample.host.plugin_view.HostAddPluginViewActivity;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.TestHostTheme);
+
+        LinearLayout rootView = new LinearLayout(this);
+        rootView.setOrientation(LinearLayout.VERTICAL);
+
+        TextView infoTextView = new TextView(this);
+        infoTextView.setText(R.string.main_activity_info);
+        rootView.addView(infoTextView);
+
+        final Spinner partKeySpinner = new Spinner(this);
+        ArrayAdapter<String> partKeysAdapter = new ArrayAdapter<>(this, R.layout.part_key_adapter);
+        partKeysAdapter.addAll(
+                Constant.PART_KEY_PLUGIN_MAIN_APP,
+                Constant.PART_KEY_PLUGIN_ANOTHER_APP
+        );
+        partKeySpinner.setAdapter(partKeysAdapter);
+
+        rootView.addView(partKeySpinner);
+
+        Button startPluginButton = new Button(this);
+        startPluginButton.setText(R.string.start_plugin);
+        startPluginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String partKey = (String) partKeySpinner.getSelectedItem();
+                Intent intent = new Intent(MainActivity.this, PluginLoadActivity.class);
+                switch (partKey) {
+                    //为了演示多进程多插件，其实两个插件内容完全一样，除了所在进程
+                    case Constant.PART_KEY_PLUGIN_MAIN_APP:
+                        intent.putExtra(Constant.KEY_PLUGIN_PART_KEY, PART_KEY_PLUGIN_BASE);
+                        break;
+                    case Constant.PART_KEY_PLUGIN_ANOTHER_APP:
+                        intent.putExtra(Constant.KEY_PLUGIN_PART_KEY, partKey);
+                        ;
+                        break;
+                }
+
+                switch (partKey) {
+                    //为了演示多进程多插件，其实两个插件内容完全一样，除了所在进程
+                    case Constant.PART_KEY_PLUGIN_MAIN_APP:
+                    case Constant.PART_KEY_PLUGIN_ANOTHER_APP:
+                        intent.putExtra(Constant.KEY_ACTIVITY_CLASSNAME, "com.tencent.shadow.sample.plugin.app.lib.gallery.splash.SplashActivity");
+                        break;
+
+                }
+                startActivity(intent);
+            }
+        });
+        rootView.addView(startPluginButton);
+
+        Button startHostAddPluginViewActivityButton = new Button(this);
+        startHostAddPluginViewActivityButton.setText("宿主添加插件View");
+        startHostAddPluginViewActivityButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, HostAddPluginViewActivity.class);
+            startActivity(intent);
+        });
+        rootView.addView(startHostAddPluginViewActivityButton);
+
+        setContentView(rootView);
+
+    }
+
+}
--- simple/src/main/java/com/ven/assists/simple/App.kt
+++ simple/src/main/java/com/ven/assists/simple/App.kt
@@ -1,21 +1,93 @@
 package com.ven.assists.simple
 
+import android.app.ActivityManager
 import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.os.Process
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+import android.webkit.WebView
 import com.blankj.utilcode.util.Utils
-import com.ven.assists.simple.step.GestureBottomTab
-import com.ven.assists.simple.step.GestureScrollSocial
-import com.ven.assists.simple.step.OpenWechatSocial
-import com.ven.assists.simple.step.PublishSocial
-import com.ven.assists.simple.step.ScrollContacts
-import com.ven.assists.stepper.StepManager
+import com.tencent.shadow.core.common.LoggerFactory
+import com.tencent.shadow.dynamic.host.DynamicRuntime
+import com.tencent.shadow.dynamic.host.PluginManager
+import com.ven.assists.simple.plugin.HostUiLayerProvider
+import java.io.File
 
 class App : Application() {
 
-    companion object{
+    companion object {
         const val TARGET_PACKAGE_NAME = "com.tencent.mm"
+        var sApp: App? = null
+
+
+        private fun setWebViewDataDirectorySuffix() {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+                return
+            }
+            WebView.setDataDirectorySuffix(getProcessName())
+        }
+
+        private fun detectNonSdkApiUsageOnAndroidP() {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+                return
+            }
+            val builder = VmPolicy.Builder()
+            builder.detectNonSdkApiUsage()
+            StrictMode.setVmPolicy(builder.build())
+        }
+
+        fun getApp(): App? {
+            return sApp
+        }
+
+
+        private fun isProcess(context: Context, processName: String): Boolean {
+            var currentProcName = ""
+            val manager =
+                context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+            for (processInfo in manager.runningAppProcesses) {
+                if (processInfo.pid == Process.myPid()) {
+                    currentProcName = processInfo.processName
+                    break
+                }
+            }
+
+            return currentProcName.endsWith(processName)
+        }
     }
+
+    private var mPluginManager: PluginManager? = null
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
+
+        detectNonSdkApiUsageOnAndroidP()
+        setWebViewDataDirectorySuffix()
+        LoggerFactory.setILoggerFactory(AndroidLogLoggerFactory())
+
+        if (isProcess(this, ":plugin")) {
+            //在全动态架构中，Activity组件没有打包在宿主而是位于被动态加载的runtime，
+            //为了防止插件crash后，系统自动恢复crash前的Activity组件，此时由于没有加载runtime而发生classNotFound异常，导致二次crash
+            //因此这里恢复加载上一次的runtime
+            DynamicRuntime.recoveryRuntime(this)
+        }
+
+        if (isProcess(this, packageName)) {
+            PluginHelper.getInstance().init(this)
+        }
+
+        HostUiLayerProvider.init(this)
+    }
+
+    fun loadPluginManager(apk: File?) {
+        if (mPluginManager == null) {
+            mPluginManager = Shadow.getPluginManager(apk)
+        }
+    }
+
+    fun getPluginManager(): PluginManager? {
+        return mPluginManager
     }
 }
