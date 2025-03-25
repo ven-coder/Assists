57262b3334e799504f1f98c472684e1da0decd9c
Ven <ven_coder_ing@126.com>
2025-03-25 08:15:00 +0800

    refactor(项目重构): 统一版本管理并优化项目结构- 在根目录 build.gradle 中统一管理所有依赖的版本 - 更新 AssistsService 类为开放类，支持继承 - 新增 SelectToSpeakService 类，继承自 AssistsService - 调整 simple 项目的依赖，使用本地模块替代远程仓库 - 移除 AndroidManifest.xml 中的冗余注释 - 统一代码格式，提高可读性

---
+++ assists/src/main/java/com/google/android/accessibility/selecttospeak/SelectToSpeakService.kt
@@ -0,0 +1,9 @@
+package com.google.android.accessibility.selecttospeak
+
+import com.ven.assists.service.AssistsService
+
+/**
+ * 使用这个无障碍服务可以解决一些应用混淆节点的问题
+ */
+class SelectToSpeakService: AssistsService() {
+}
