19142886cf3ea1719f8ae0460dedf27a7250f5c3
Ven <ven_coder_ing@126.com>
2025-07-07 22:44:52 +0800

    fix: 重复步骤如果不传递数据则使用当前重复步骤的数据

--- assists/src/main/java/com/ven/assists/stepper/StepOperator.kt
+++ assists/src/main/java/com/ven/assists/stepper/StepOperator.kt
@@ -95,7 +95,7 @@ class StepOperator(
 
             Step.repeat -> {
                 repeatCount++
-                StepManager.execute(implClassName, step, delay = nextStep.delay, data = data)
+                StepManager.execute(implClassName, step, delay = nextStep.delay, data = if (nextStep.data == null) data else nextStep.data)
             }
 
             else -> {
